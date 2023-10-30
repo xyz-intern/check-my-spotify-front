dfc67481e3a1afe7aa2d8638863164a39dbc7de2
ksj051203 <ksj054703@gmail.com>
2023-10-30 21:13:16 +0900

    feat: 곡 전환시 Current Playing Track 자동 요청

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,68 @@
-const Login = () => {
-    function generateRandomString(length) {
-        let result = '';
-        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-        return result;
-    }
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-    const axiosRequest = () => {
-        let state = generateRandomString(16); 
-        let scope = 'user-read-private user-read-email';
-      
+const Login = () => {
+  const [duration_ms, setDurationMs] = useState(null);
+
+  function generateRandomString(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  }
+
+  const axiosRequest = () => {
+  let state = generateRandomString(16); 
+        let scope = 'user-read-private user-read-email user-read-currently-playing user-read-playback-state';
         let client_id = process.env.REACT_APP_CLIENT_ID;
         let redirect_uri = process.env.REACT_APP_REDIRECT_URI;
-    
+
         let link = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}&state=${state}`;
-        
+
         window.location.href = link;
+  }
+
+  const fetchTrack = async () => {
+    try {
+      const userId = "dlatldhs";
+      const response = await axios.get(`http://localhost:3000/apis/getTrack/${userId}`);
+      const newDurationMs = response.data;
+      setDurationMs(newDurationMs);
+    } catch (error) {
+      console.error(error);
     }
-    return <button herf='' onClick={axiosRequest}>Spotify Login !!!</button>
-}
+  };
+
+//   useEffect(() => {
+//     // 최초 렌더링 시 fetchTrack 호출
+//     fetchTrack();
+//   }, []);
+
+  useEffect(() => {
+    // duration_ms가 변경될 때마다 fetchTrack을 호출하는 부분 유지
+    if (duration_ms !== null) {
+      const timerId = setTimeout(() => {
+        fetchTrack();
+      }, duration_ms);
+
+      // cleanup 함수를 이용하여 타이머 해제
+      return () => {
+        clearTimeout(timerId);
+      };
+    }
+  }, [duration_ms]);
+
+  return (
+    <div>
+      <button onClick={axiosRequest}>
+        Spotify Login !!!
+      </button>
+      {!duration_ms && <button onClick={fetchTrack}>재생하기</button>}
+    </div>
+  );
+};
+
 export default Login;
+
--- src/components/Redirection.js
+++ src/components/Redirection.js
@@ -2,29 +2,37 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const Redirection = () => {
-    
+
     const code = new URL(document.location.toString()).searchParams.get('code');
     const state = new URL(document.location.toString()).searchParams.get('state');
-    console.log(state);
+    console.log(code, state);
 
     let is_throw = false;
     let is_error = false;
+
     
+
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_SERVER_ADDRESS}?code=${code}&state=${state}`)
-        .then((r) => {
-            console.log(r.data);
-            console.log('good');
-            is_throw = true;
-        })
-        .catch((error) => {
-            // Handle the error here
-            console.error("Error occurred:", error);
-            is_error=true;
-        });
+            .then((r) => {
+                console.log(r.data);
+                console.log('good');
+                is_throw = true;
+            })
+            .catch((error) => {
+                // Handle the error here
+                console.error("Error occurred:", error);
+                is_error = true;
+            });
     }, []);
-  
-    return <div>{ is_throw ? (is_error ? "데이터를 전송 중입니다." : "에러 발생") : "데이터를 성공적으로 전송하였습니다." }</div>;
+
+
+
+    return (
+        <div>
+            <div>{is_throw ? (is_error ? "데이터를 전송 중입니다." : "에러 발생") : "데이터를 성공적으로 전송하였습니다."}</div>
+        </div>
+    )
 };
-  
+
 export default Redirection;
