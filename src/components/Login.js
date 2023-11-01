1f111bf2152c401bda3368e95af23e9b13b45e2d
ksj051203 <ksj054703@gmail.com>
2023-11-01 16:03:34 +0900

    feat: get Cookies value

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Cookies from 'js-cookie';
 
+const userId = Cookies.get('userId');
 const Login = () => {
+
   const [duration_ms, setDurationMs] = useState(null);
 
   function generateRandomString(length) {
@@ -14,19 +17,18 @@ const Login = () => {
   }
 
   const axiosRequest = () => {
-  let state = generateRandomString(16); 
-        let scope = 'user-read-private user-read-email user-read-currently-playing user-read-playback-state';
-        let client_id = process.env.REACT_APP_CLIENT_ID;
-        let redirect_uri = process.env.REACT_APP_REDIRECT_URI;
-
-        let link = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}&state=${state}`;
-
-        window.location.href = link;
+    let state = generateRandomString(16);
+    let scope = 'user-read-private user-read-email user-read-currently-playing user-read-playback-state user-modify-playback-state';
+    let client_id = process.env.REACT_APP_CLIENT_ID;
+    let redirect_uri = process.env.REACT_APP_REDIRECT_URI;
+    let link = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}&state=${state}`;
+    window.location.href=link
   }
 
+
   const fetchTrack = async () => {
+    const userId = Cookies.get('userId');
     try {
-      const userId = "dlatldhs";
       const response = await axios.get(`http://localhost:3000/apis/getTrack/${userId}`);
       const newDurationMs = response.data;
       setDurationMs(newDurationMs);
@@ -35,10 +37,6 @@ const Login = () => {
     }
   };
 
-//   useEffect(() => {
-//     // 최초 렌더링 시 fetchTrack 호출
-//     fetchTrack();
-//   }, []);
 
   useEffect(() => {
     // duration_ms가 변경될 때마다 fetchTrack을 호출하는 부분 유지
@@ -62,7 +60,7 @@ const Login = () => {
       {!duration_ms && <button onClick={fetchTrack}>재생하기</button>}
     </div>
   );
-};
+  }
 
 export default Login;
 
--- src/components/Redirection.js
+++ src/components/Redirection.js
@@ -1,19 +1,17 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { Cookies } from 'react-cookie';
 
 const Redirection = () => {
-
+    const cookies = new Cookies()
     const code = new URL(document.location.toString()).searchParams.get('code');
     const state = new URL(document.location.toString()).searchParams.get('state');
-    console.log(code, state);
 
     let is_throw = false;
     let is_error = false;
 
-    
-
     useEffect(() => {
-        axios.get(`${process.env.REACT_APP_SERVER_ADDRESS}?code=${code}&state=${state}`)
+        axios.get(`http://localhost:3000/callback?code=${code}&state=${state}`, {withCredentials: true})
             .then((r) => {
                 console.log(r.data);
                 console.log('good');
@@ -24,9 +22,9 @@ const Redirection = () => {
                 console.error("Error occurred:", error);
                 is_error = true;
             });
+            
+        
     }, []);
-
-
 
     return (
         <div>
