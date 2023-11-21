b82e0552673666831e8d7cbf3792d3d332eebe61
dlatldhs <sionzz1713@gmail.com>
2023-11-21 16:56:57 +0900

    add useConext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext ,useState } from 'react';
 import Login from "./components/Login/Login";
 import {
   BrowserRouter,
@@ -7,25 +7,33 @@ import {
 } from "react-router-dom";
 import Artist from './components/Statistics/Favorite/Artists/Artist'
 import FavoriteSong from './components/Statistics/Favorite/Song/Song'
-import Header from './components/Header/Header'
 import LastSong from './components/Statistics/LastSong/LastSong';
+import Header from './components/Header/Header'
 import Redirection from './components/Login/Redirection'
 
-const code = window.location.search;
+export const AppContext = createContext();
+
+let code = window.location.search;
 function App() {
+
+  const [items, setItem] = useState(code);
+
   return (
     <div className="App">
-      {code ? <Redirection code={code} /> : <Login />}
-
-      <BrowserRouter>
+      {/* how to using: const { items, setItem } = useContext(AppContext); */}
+      <AppContext.Provider value={{ items, setItem }}>
+        {code ? <Redirection code={code} /> : <Login />}
+        <BrowserRouter>
         <Header />
-          <Routes>
-            <Route path="/last/song" element={<LastSong /> } />
-            <Route path="/favorite/song" element={<FavoriteSong />} />
-            <Route path="/favorite/artist" element={<Artist />} />
-            {/* <Route path="/login" element={<Login />} /> */}
-          </Routes>
-      </BrowserRouter> 
+        <Routes>
+          <Route path="/last/song" element={<LastSong /> } />
+          <Route path="/favorite/song" element={<FavoriteSong />} />
+          <Route path="/favorite/artist" element={<Artist />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        </BrowserRouter>
+      </AppContext.Provider>
+
     </div>
   );
 }
