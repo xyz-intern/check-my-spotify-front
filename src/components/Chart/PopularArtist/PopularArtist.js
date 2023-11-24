f730a2711cf7953877136960d7005f5ca08c093e
dlatldhs <sionzz1713@gmail.com>
2023-11-24 14:43:36 +0900

    test image slider

--- src/components/Chart/PopularArtist/PopularArtist.js
+++ src/components/Chart/PopularArtist/PopularArtist.js
@@ -2,52 +2,28 @@ import axios from 'axios';
 import { useEffect, useState , useRef } from 'react';
 import ChartList from '../ChartList/ChartList';
 import styled, { keyframes } from 'styled-components';
-
-
-// const ScrollContainer = styled.div`
-//     display: flex;
-//     overflow-x: auto;
-//     /* /* scroll-snap-type: x mandatory; */
-//     -webkit-overflow-scrolling: touch;
-//     scroll-behavior: smooth; 
-// `;
+import { gsap } from 'gsap/gsap-core';
 
 const ScrollContainer = styled.div`
     width: 100%;
     overflow: hidden;
-    position: reletive;
-
-    &::after {
-        width: 1000%;
-        
-    }
-`;
-
+    position: relative;
+    display: flex;
+    top: 5%;
+`
 const BannerMove = keyframes`
-    0% {
-        transform: translateX(0);
-    }
-    100% {
-        transform: translateX(-50%);
-    }
+    from { transform: translateX(0px); }
+    to   { transform: translateX(-22560px); }
 `
 
 const Article = styled.div`
     display: flex;
-    width: 3000%;
-    animation: ${BannerMove} 100s linear infinite;
-    white-space: nowrap;
-    position: reletive;
+    width: 400%;
+    white-space: nowrap;    
+    animation: ${BannerMove} 400s linear infinite;
+    // animation-iteration-count: infinite;
+    position: relative;
 `
-    
-// const ScrollItem = styled.div`
-//     flex: none;
-//     scroll-snap-align: start;
-//     margin-right: 100px;
-//     max-width: 350px;
-//     width: 100%;
-//     height: 100%;
-// `;
 
 const ScrollItem = styled.div`
     display: flex;    
@@ -58,14 +34,14 @@ const ScrollItem = styled.div`
     height: 100%;
 `;
 
-
 const PopularArtist = () => {
     const scrollRef = useRef(null);
     const [popularArtist, setPopularArtist] = useState([]);
-
+    
     useEffect(() => {
         fetchLastSong();
     }, []);
+
 
     const fetchLastSong = () => {
         axios.get('http://192.168.0.133:3000/top/artist')
@@ -78,37 +54,33 @@ const PopularArtist = () => {
     }
 
     return (
-        // version 1
-        // <ScrollContainer ref={scrollRef}>
-        //     {popularArtist.map((data) => (
-        //         <ScrollItem key={data.rank}>
-        //             <ChartList
-        //                 type="artist"
-        //                 rank={data.rank}
-        //                 artist={data.artist}
-        //                 imageUri={data.imageUri}
-        //             />
-        //         </ScrollItem>
-        //     ))}
-        // </ScrollContainer>
 
-        // version 2
-        <ScrollContainer ref={scrollRef}>
-            <Article>
-            {popularArtist.map((data) => (
-                <ScrollItem key={data.rank}>
-                    <ChartList
-                        type="artist"
-                        rank={data.rank}
-                        artist={data.artist}
-                        imageUri={data.imageUri}
-                    />
-                </ScrollItem>
-            ))}
-            </Article>
-        </ScrollContainer>
+            <ScrollContainer ref={scrollRef}>
+                <Article>
+                {popularArtist.map((data) => (
+                    <ScrollItem key={data.rank} className='box'>
+                        <ChartList
+                            type="artist"
+                            rank={data.rank}
+                            artist={data.artist}
+                            imageUri={data.imageUri}
+                            />
+                    </ScrollItem>
+                ))}
+
+                {popularArtist.map((data) => (
+                    <ScrollItem key={data.rank+50} className='box'>
+                        <ChartList
+                            type="artist"
+                            rank={data.rank}
+                            artist={data.artist}
+                            imageUri={data.imageUri}
+                            />
+                    </ScrollItem>
+                ))}
+                </Article>
+            </ScrollContainer>
 
     );
 }
-
 export default PopularArtist;
